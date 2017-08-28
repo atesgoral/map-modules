d5af22dbe30ed4c7f8ee58831b3d2b7fb511fb7f
Ates Goral <ates.g@myplanet.com>
2017-08-28 02:42:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # map-modules
-Expanded ASN.1 modules for Mobile Application Part (MAP) (3GPP TS 29.002) in JSON format
+
+Expanded ASN.1 modules for Mobile Application Part (MAP) (3GPP TS 29.002) in JSON format. Just the OPERATION definitions.
+
+Converted using [asn1exp](https://www.npmjs.com/package/asn1exp) from http://www.3gpp.org/ftp/specs/archive/29_series/29.002/ASN.1/.
+
+The files are under the dist folder. Just load up the ones you need:
+
+```
+const mapMobileServiceOperations = require('map-modules/dist/MAP-MobileServiceOperations.EXP.min.json');
+```
---
+++ convert.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const asn1exp = require('asn1exp');
+const parseArgs = require('minimist');
+const getStdin = require('get-stdin');
+
+const argv = parseArgs(process.argv.slice(2), {
+  boolean: 'min'
+});
+
+const fileRead = process.argv[2]
+  ? Promise.resolve(fs.readFileSync(process.argv[2], 'UTF-8'))
+  : getStdin();
+
+fileRead
+  .then((s) => {
+    const parsed = asn1exp.parse(s);
+
+    if (argv.min) {
+      console.log(JSON.stringify(parsed.operations));
+    } else {
+      console.log(JSON.stringify(parsed.operations, null, 2));
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
